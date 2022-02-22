0aa7b7f1f03b64944628773bd5d260f1583bac1d
Evert Pot <me@evertpot.com>
2022-02-21 20:51:53 -0500

    Changes for episode 9

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { ClientNew } from './client-new';
+export { Client } from './client';
 export { EntryNew } from './entry-new';
 export { PersonNew } from './person-new';
 export { ProjectNew } from './project-new';
