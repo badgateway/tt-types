50e9a301d51ebc8487fe42558ce59587b9e4e54e
Evert Pot <me@evertpot.com>
2022-03-03 22:53:25 -0500

    Adding types for 'person' and 'entry'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-export { ClientNew } from './client-new';
 export { Client } from './client';
+export { ClientNew } from './client-new';
+
+export { Entry } from './entry';
 export { EntryNew } from './entry-new';
+
+export { Person } from './person';
 export { PersonNew } from './person-new';
-export { ProjectNew } from './project-new';
+
 export { Project } from './project';
+export { ProjectNew } from './project-new';
